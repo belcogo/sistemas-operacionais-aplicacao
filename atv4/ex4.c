869a5ae6f2ddd8aa2b77d6dc457016a4213041b0
belcogo <bel.cogo27@gmail.com>
2022-09-02 19:37:00 -0300

    feat: adiciona solução para problemas 3 e 4

--- atv4/ex4.c
+++ atv4/ex4.c
@@ -4,29 +4,35 @@
 #include <string.h>
 #include <sys/wait.h>
 
+#define status 0
+
 void forker(int n);
 
 int main(int argc, char *argv[]) {
     long qtd_processes = strtol(argv[1], NULL, 10);
 
-    printf("Meu ID: %d\n", getpid());
     forker(qtd_processes);
     return 0;
 }
 
-// fonte: https://stackoverflow.com/questions/876605/multiple-child-process
 void forker(int n) {
     pid_t pid;
+    
+    if (n == 0) {
+        printf("Sou o último processo. Meu ID: %d -- ID do meu pai: %d\n", getpid(), getppid());
+    }
 
     if(n > 0) {
         if ((pid = fork()) < 0) {
             perror("fork");
         }
         else if (pid == 0) {
-            printf("Meu ID: %d -- ID do meu pai: %d\n", getpid(), getppid());
-        }
-        else if(pid > 0) {
+            
             forker(n - 1);
+            
+        } else {
+            waitpid(pid, status, 0);
+            printf("Meu ID: %d -- ID do meu pai: %d\n", getpid(), getppid());
         }
     }
 }
