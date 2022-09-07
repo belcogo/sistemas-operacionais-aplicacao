44938b38d837c62c14bd64480f5698fbaaf1d10e
Bel Cogo <bel.cogo@cwi.com.br>
2022-09-07 17:43:31 -0300

    WIP: add solution for problem no #1

--- atv5/ex1.c
+++ atv5/ex1.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ECHOMAX 1024
+#define ECHOMAX 5120
 
 void aaa(char command[ECHOMAX], int file_descriptor[2]);
 
@@ -16,17 +16,12 @@ int main() {
 
     if (pipe(file_pipes) == 0) {
         fork_result = fork();
-        // if (fork_result == -1) {
-        //     fprintf(stderr, "Fork failure");
-        //     exit(EXIT_FAILURE);
-        // }
 
-        printf("%d\n", fork_result);
+        // printf("%d\n", fork_result);
 
         if (fork_result > 0) {
-            printf("Oi 3");
             close(file_pipes[0]);
-            aaa("curl https://g1.globo.com -o a", file_pipes);
+            aaa("curl https://g1.globo.com -o a --silent", file_pipes);
             close(file_pipes[1]);
         }
 
@@ -35,15 +30,17 @@ int main() {
             close(0);
             close(file_pipes[1]);
             dup(file_pipes[0]);
-            printf("Oi\n");
-            char *readBuffer[ECHOMAX];
-            ssize_t n = read(file_pipes[0], readBuffer, sizeof(readBuffer));
-            readBuffer[n] = 0;
+            printf("> grep -oi 'coronavirus' | wc -l\n");
+            // system("cat a");
+            system("grep -oi 'coronavirus' | wc -l");
+            // char *readBuffer[ECHOMAX];
+            // ssize_t n = read(file_pipes[0], readBuffer, sizeof(readBuffer));
+            // readBuffer[n] = 0;
             close(file_pipes[0]);
-            if (n > 0) {
-                printf("%s", readBuffer);
-            }
-            // system("rm a.txt");
+            // if (n > 0) {
+            //     printf("%s", readBuffer);
+            // }
+            // system("rm a");
         }
     }
 
@@ -51,30 +48,29 @@ int main() {
 }
 
 void aaa(char command[ECHOMAX], int file_descriptor[2]) {
-    printf("Oi 2");
     char result[ECHOMAX];
     system(command);
     get_result(result);
-    // write(file_descriptor[1], result, strlen(result));
+    write(file_descriptor[1], result, strlen(result));
 }
 
 char *get_result(char result[ECHOMAX]) {
 	FILE *fp;
 	char buffer[ECHOMAX];
-    // char aaa[ECHOMAX];
-    
 	fp = fopen("a", "r");
-	// int i = 0;
-	// while (1) {
-    //     // printf("%s\n", fgetc(fp));
-	// 	// buffer[i] = fgetc(fp); // reading the file
-	// 	if (buffer[i] == EOF) {
-    //         break;
-    //     }
-	// 	++i;
-	// }
-    fgets(buffer, ECHOMAX, fp);
-	fclose(fp);
+	int i = 0;
+	while (i) {
+		fgets(buffer[i], sizeof(fp), fp);
+        if (buffer[i] == EOF) {
+            break;
+        }
+        // printf("%s\n", buffer[i]);
+		++i;
+	}
+    // fgets(buffer, sizeof(fp), fp);
+	// fclose(fp);
+    strcat(buffer, "\n");
     strcpy(result, buffer);
+    printf("OLHA AQUI>>>>>>>>>>>>>> \n%s\n", buffer);
 	return result;
 }
