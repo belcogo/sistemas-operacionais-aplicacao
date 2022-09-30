a1de2b0a9e7a1bd2e8418792a0dd3227aab3fafb
Bel Cogo <bel.cogo@cwi.com.br>
2022-09-30 17:27:17 -0300

    feat: add solution for problem no #1

--- atv8/ex1.c
+++ atv8/ex1.c
@@ -5,11 +5,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 10;
 int sum = 0;
-int index = 0;
-float medias[10];
 int current_media = 0;
+int idx = 0;
+int linear = 0;
+float medias[6];
+float sums[10];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t condicao_calculate_media = PTHREAD_COND_INITIALIZER;
 pthread_cond_t condicao_calculate_medias_media = PTHREAD_COND_INITIALIZER;
@@ -20,18 +21,21 @@ void* thread_P();
 void* thread_M();
 
 void espera() {
-    usleep((useconds_t) 1);
+    sleep(1);
 }
 
 void* thread_C() {
     while(1) {
         pthread_mutex_lock(&mutex);
-        for (index = 0; index < 10; index++) {
-            int valor = random() % 5000;
-            sum += valor;
-            printf("C| %d |\n", sum);
-            espera();
+        for (int k = 0; k < 6; k++) {
+            sums[k] = 0;
+            for (int i = 0; i < 10; i++) {
+                int valor = random() % 5000;
+                sums[k] += valor;
+                espera();
+            }
         }
+        idx = 1;
         pthread_mutex_unlock(&mutex);
         pthread_cond_signal(&condicao_calculate_media);
         break;
@@ -41,50 +45,56 @@ void* thread_C() {
 void* thread_P() {
     while(1) {
         pthread_mutex_lock(&mutex);
-        while(index != 10) {
+        while(idx == 0) {
             pthread_cond_wait(&condicao_calculate_media, &mutex);
         }
-        medias[current_media] = sum / 10;
-        printf("P| %f |\n", medias[current_media]);
-        sum = 0;
-        current_media++;
-        pthread_mutex_unlock(&mutex);
-        if (current_media == 9) {
-            pthread_cond_signal(&condicao_calculate_medias_media);
-            break;
+        for (int i = 0; i < 6; i++) {
+            medias[current_media] = sums[current_media] / 10;
+            current_media++;
         }
+        pthread_mutex_unlock(&mutex);
+        break;
     }
+    pthread_cond_signal(&condicao_calculate_medias_media);
 }
 
 void* thread_M() {
     while(1) {
         pthread_mutex_lock(&mutex);
-        while( current_media != 9) {
+        while(current_media < 1) {
             pthread_cond_wait(&condicao_calculate_medias_media, &mutex);
         }
-        float medias_media;
-        float medias_sum = 0;
+        printf("\n_______________________________\n*******************************\nLINEAR = %d\n_______________________________\nMÉDIAS ABAIXO DA MÉDIA GERAL\n", linear);
         for (int i = 0; i < current_media; i++) {
-            medias_sum += medias[i];
-        }
-        medias_media = medias_sum / current_media;
-        printf("M| %f |\n", medias_media);
-        for (int i = 0; i < current_media; i++) {
-            if (medias[i] < medias_media) {
-                printf("Media nro #%d = %f\nMedia geral = %f\n", i, medias[i], medias_media);
+            if (medias[i] < linear) {
+                printf("Media nro #%d = %f\n", i, medias[i]);
             }
         }
+        printf("_______________________________");
         pthread_mutex_unlock(&mutex);
         break;
     }
 }
 
-int main() {
-    pthread_t threads[3];
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        printf ("Uso correto: %s <linear>\n\n", argv[0]);
+        return 1;
+    }
+
+    pthread_mutex_lock(&mutex);
+    linear = atol(argv[1]);
+    pthread_mutex_unlock(&mutex);
+
+    pthread_t tids[3];
     srand(time(NULL));
-    pthread_create(&threads[0], NULL, &thread_C, NULL);
-    pthread_create(&threads[1], NULL, &thread_P, NULL);
-    pthread_create(&threads[2], NULL, &thread_M, NULL);
+
+    printf("MÉDIA DE TEMPERATURAS MEDIDAS DURANTE 1MINUTO\n");
+
+    pthread_create(&tids[0], NULL, &thread_C, NULL);
+    pthread_create(&tids[1], NULL, &thread_P, NULL);
+    pthread_create(&tids[1], NULL, &thread_M, NULL);
 
     pthread_exit(NULL);
     return 0;
