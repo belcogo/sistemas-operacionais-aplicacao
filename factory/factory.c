f56fa071b0373bfbaa09ad4f9c015ae0b37f337e
belcogo <bel.cogo27@gmail.com>
2022-10-07 17:54:14 -0300

    fix: corrige comportamento encerrar producao

--- factory/factory.c
+++ factory/factory.c
@@ -8,6 +8,14 @@
 #include <signal.h>
 #include "Queue.h"
 
+// config
+#define ECHOMAX 250
+
+// styling
+#define DEFAULT_SEPARATOR "________________________________"
+#define DETAIL_SEPARATOR "--------------------------------"
+#define ESPECIAL_SEPARATOR "********************************"
+
 // states
 #define PRODUCING 1
 #define CONSUMING -1
@@ -46,9 +54,11 @@ Queue products;
 // def product to be used in peek and dequeue
 product_t prd_current;
 
-// controller for production and consuming numbers
+// controllers for production and consuming numbers
 int controller[3][3][3] = {0};
-int consumed_items = 0;
+int consumed_items_count = 0;
+int products_count = 0;
+int production_time = 0;
 
 // array of types to be used by the producers and consumers
 char types[N] = {A, B, C, A, B};
@@ -61,15 +71,11 @@ int qualities[3] = {HIGH, MEDIUM, LOW};
 
 // def semaphores
 sem_t mutex;
-sem_t producer_sem;
 
-static volatile bool keepRunning = true;
-
-// def array of states for the production mat control
+// def array of states for the production line control
 int state[N];
-int total_sum = 0;
 
-// threads
+// threads CONTROL
 pthread_t threads[N];
 int result;
 void *thread_result;
@@ -92,37 +98,27 @@ int main() {
 	// initializing queue
 	queueInit(&products, sizeof(int));
 
-	// initializing semaphores and states
+	// initializing semaphores
 	result = sem_init(&mutex, 0, 1);
 	if (result != 0) {
 		perror("init mutex");
 		exit(EXIT_FAILURE);
 	}
 
-	result = sem_init(&producer_sem, 0, 1);
-	if (result != 0) {
-		perror("init producer_sem");
-		exit(EXIT_FAILURE);
-	}
-
-	for (int i = 0; i < 3; i++) {
+  // initializing states
+	for (int i = 0; i < N; i++) {
 		state[i] = ON_HOLD;
-		// result = sem_init(&semaphores[i], 0, 0);
-		// if (result != 0) {
-		// 	perror("init sem");
-		// 	exit(EXIT_FAILURE);
-		// }
 	}
 
 	int loop_control = 0;
 
-	// while (loop_control < N) {
+  printf("Abrindo a fábrica...\nCaso queira encerrar o expediente pressione ctrl+c\n\nIniciando produção...\n\n");
+
+  // create threads
 	for (int i = 0; i < N; i++) {
 		if (i >= 3) {
-			// create thread for consumer
 			result = pthread_create(&threads[i], NULL, &consumer, i);
 		} else {
-			// create thread for producer
 			result = pthread_create(&threads[i], NULL, &producer, i);
 		}
 
@@ -132,6 +128,7 @@ int main() {
 		}
 	}
 
+  // join threads
 	for (int i = 0; i < N; i++) {
 		result = pthread_join(threads[i], &thread_result);
 
@@ -149,75 +146,74 @@ void *producer(int i) {
 	// define producer type
 	char type = types[i];
 
-	while (keepRunning) {
+	while (1) {
 		if (getQueueSize(&products) > 0) {
 			sleep(1);
 		}
+
 		int loop_control = 0;
-		printf("INICIO PRODUTOR - TIPO %c\n", type);
+
 		// define number of products to be produced
 		int number_of_products = generate_random_number(NULL);
-		printf("%d\n", number_of_products);
 
 		while (loop_control < number_of_products) {
+      // get random option for price and quality
 			int price_idx = generate_random_number(3);
 			int quality_idx = generate_random_number(3);
+
+      // create product object
 			product_t new_product;
 			new_product.type = type;
 			new_product.price_range = prices[price_idx];
 			new_product.quality = qualities[quality_idx];
-			new_product.produce_time = generate_random_number(5);
-			new_product.consume_time = new_product.produce_time * 0.5;
+			new_product.produce_time = generate_random_number(NULL);
+			new_product.consume_time = new_product.produce_time * 0.8;
+
+      production_time+= new_product.produce_time;
+      // production_time+= new_product.consume_time;
+
 			// verify if there is any producer or consumer already active
 			int result = check_status(i);
 
 			// keep in wait loop until there is no producer/consumer active
 			while (result >= 0) {
 				// wait post from active producer/consumer
-				// sem_wait(&semaphores[result]);
 				sem_wait(&mutex);
 				result = check_status(i);
 			}
 
 			// wait time defined for product production
-			// sleep(new_product.produce_time);
-			sleep(1);
+			sleep(new_product.produce_time);
+
+			// add to queue
 			enqueue(&products, &new_product);
+
 			// post semaphore to free critical section
-  		sem_post(&mutex);
+  			sem_post(&mutex);
+
 			printf("\n\nPRODUTO PRODUZIDO:\n");
 			print_product((product_t *)&new_product);
-			controller[i][quality_idx][price_idx] += 1;
-			state[i] = ON_HOLD;
 
-			if (getQueueSize(&products) == 1) {
-				// post semaphore for consumers
-				sem_post(&producer_sem);
-			}
-			loop_control++;
-			
+			// add one unity to specific type - quality - price range
+			controller[i][quality_idx][price_idx] += 1;
+
+			// set state to on hold to signal that is not active anymore
+			state[i] = ON_HOLD;
+			loop_control++;	
 		}
 	}
-
-	
-  printf("FIM PRODUTOR - TIPO %c\n", type);
-	int queue_size = getQueueSize(&products);
-	printf("\nqueue size prod%d\n", queue_size);
 }
 
 void *consumer(int i) {
 	// define consumer type
 	char type = types[i];
-	while(keepRunning) {
+
+	while(1) {
 		// wait liberation for critical section
 		sem_wait(&mutex);
 		
 		// verify if there is any producer or consumer already active
 		int result = check_status(i);
-
-		// while (getQueueSize(&products) <= 0) {
-		// 	sem_wait(&producer_sem);
-		// }
 
 		// keep in wait loop until there is no producer/consumer active
 		while (result >= 0) {
@@ -225,8 +221,6 @@ void *consumer(int i) {
 			sem_wait(&mutex);
 			result = check_status(i);
 		}
-
-		printf("INÍCIO CONSUMIDOR - TIPO %c\n", type);
 
 		// keep consuming while there is products in queue
 		while (getQueueSize(&products) > 0) {
@@ -236,21 +230,20 @@ void *consumer(int i) {
 			if ((prd_current.type == A && type != A) || (prd_current.type != A && type == A)) break;
 
 			// wait defined consuming type for product
-			// sleep(prd_current.produce_time);
-			sleep(1);
+			sleep(prd_current.consume_time);
+
+			// remove from queue
 			dequeue(&products, &prd_current);
+
 			printf("\nPRODUTO CONSUMIDO:\n");
 			print_product((product_t *)&prd_current);
 		}
 
 		state[i] = ON_HOLD;
+
 		// post semaphore to free critical section
 		sem_post(&mutex);
 	}
-
-	
-  
-  printf("FIM CONSUMIDOR - TIPO %c\n", type);
 }
 
 int generate_random_number(int factor) {
@@ -283,12 +276,10 @@ int check_status(int idx) {
 }
 
 float calc_media(int value) {
-	return (float)(100 * value) / total_sum;
+	return (float)(100 * value) / products_count;
 }
 
 void show_statistics() {
-	
-
 	int sum_a = 0;
 	int sum_b = 0;
 	int sum_c = 0;
@@ -301,43 +292,62 @@ void show_statistics() {
 	int sum_silver = 0;
 	int sum_bronze = 0;
 
+  // iterate over types
 	for (int type = 0; type < 3; type++) {
+    // iterate over quality options
     for (int quality = 0; quality < 3; quality++) {
+      // iterate over price range options
       for (int price = 0; price < 3; price++) {
 				int current_count = controller[type][quality][price];
+
 				char type_value = types[type];
 				int quality_value = qualities[quality];
 				int price_value = prices[price];
 
+        // calculates total for each type
 				switch(type_value) {
 					case A: sum_a += current_count; break;
 					case B: sum_b += current_count; break;
 					case C: sum_c += current_count; break;
 				}
 
+        // calculates total for each quality option
 				switch(quality_value) {
 					case HIGH: sum_high += current_count; break;
 					case MEDIUM: sum_medium += current_count; break;
 					case LOW: sum_low += current_count; break;
 				}
 
+        // calculates total for each price range option
 				switch(price_value) {
 					case GOLD: sum_gold += current_count; break;
 					case SILVER: sum_silver += current_count; break;
 					case BRONZE: sum_bronze += current_count; break;
 				}
-
-				total_sum += current_count;
+        
+        // update total items count
+				products_count += current_count;
       }
     }
   }
-	consumed_items = total_sum - getQueueSize(&products);
 
-	printf("\n**************************************\n______________________________________\n");
-	printf("%-20s%-20s%-12s%-12s%-12s%-16s%-16s%-16s%-12s%-12s%-12s\n", "PRODUTOS PRODUZIDOS", "PRODUTOS CONSUMIDOS", "TIPO A(%)", "TIPO B(%)", "TIPO C(%)", "QUALI. ALTA(%)", "QUALI. MEDIA(%)", "QUALI. BAIXA(%)", "GOLD(%)", "SILVER(%)", "BRONZE(%)"); 
-	printf("%-20d%-20d%-12.2f%-12.2f%-12.2f%-16.2f%-16.2f%-16.2f%-12.2f%-12.2f%-12.2f\n", total_sum, consumed_items, calc_media(sum_a), calc_media(sum_b), calc_media(sum_c), calc_media(sum_high), calc_media(sum_medium), calc_media(sum_low), calc_media(sum_gold), calc_media(sum_silver), calc_media(sum_bronze)); 
-
-	printf("\n**************************************\n______________________________________\n");
+  // get consumed items count
+	consumed_items_count = products_count - getQueueSize(&products);
+  float media_production_time = production_time / products_count;
+  printf("\n\n%s%s\n", DEFAULT_SEPARATOR, DEFAULT_SEPARATOR);
+  printf("%s%s\n", ESPECIAL_SEPARATOR, ESPECIAL_SEPARATOR);
+  printf("%-20s%-30s\n", "TOTAL PRODUZIDO", "TEMPO MÉDIO PRODUCAO (seg)");
+  printf("%-20d%-30.2f\n", products_count, media_production_time);
+  printf("%s%s\n", DETAIL_SEPARATOR, DETAIL_SEPARATOR);
+  printf("%-20s%-20s%-20s\n", "TIPO A(%)", "TIPO B(%)", "TIPO C(%)");
+  printf("%-20d%-20.2f%-20.2f\n", calc_media(sum_a), calc_media(sum_b), calc_media(sum_c));
+  printf("%s%s\n", DETAIL_SEPARATOR, DETAIL_SEPARATOR);
+  printf("%-20s%-20s%-20s\n", "QUALI. ALTA(%)", "QUALI. MEDIA(%)", "QUALI. BAIXA(%)");
+  printf("%-20d%-20.2f%-20.2f\n", calc_media(sum_high), calc_media(sum_medium), calc_media(sum_low));
+  printf("%s%s\n", DETAIL_SEPARATOR, DETAIL_SEPARATOR);
+  printf("%-20s%-20s%-20s\n", "GOLD(%)", "SILVER(%)", "BRONZE(%)");
+  printf("%-20d%-20.2f%-20.2f\n", calc_media(sum_gold), calc_media(sum_silver), calc_media(sum_bronze));
+  printf("%s%s\n", DEFAULT_SEPARATOR, DEFAULT_SEPARATOR);
 }
 
 
@@ -353,12 +363,13 @@ void end_production_line() {
 		state[0] = ON_HOLD;
 		state[1] = ON_HOLD;
 		state[2] = ON_HOLD;
-		
 	}
 
 	while(getQueueSize(&products) > 0) {
 		sem_post(&mutex);
 	}
+
+  sem_wait(&mutex);
 
 	for(int i = 3; i < N; i++) {
 			pthread_cancel(threads[i]);
@@ -370,5 +381,23 @@ void end_production_line() {
 }
 
 void print_product(product_t *product) {	
-	printf("\n**************************************\n______________________________________\nTIPO | QUALI. | PRECO\n--------------------------------------\n%c   | %d   | %d\n______________________________________\n\n", product->type, product->quality, product->price_range);
+  char value_quality[ECHOMAX];
+  char value_price[ECHOMAX];
+
+  switch(product->quality) {
+    case HIGH: strcpy(value_quality, "Alta"); break;
+    case MEDIUM: strcpy(value_quality, "Média"); break;
+    case LOW: strcpy(value_quality, "Alta"); break;
+  }
+
+  switch(product->price_range) {
+    case GOLD: strcpy(value_price, "R$ Gold"); break;
+    case SILVER: strcpy(value_price, "R$ Silver"); break;
+    case BRONZE: strcpy(value_price, "R$ Bronze"); break;
+  }
+  printf("%s\n", DEFAULT_SEPARATOR);
+  printf("%-6s%-12s%-14s\n", "TIPO", "QUALIDADE", "FAIXA DE PREÇO"); 
+  printf("%s\n", DETAIL_SEPARATOR);
+  printf("%-6c%-12s%-14s\n", product->type, value_quality, value_price);
+  printf("%s\n", DEFAULT_SEPARATOR);
 }
